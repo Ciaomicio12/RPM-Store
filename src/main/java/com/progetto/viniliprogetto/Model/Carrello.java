8ff3f8e71f08a809d29c19a3cc4ede90d9a51ff4
carmi <c.aiello12@studenti.unisa.it>
2025-05-02 15:25:32 +0000

    Aggiunta delle seguenti classi:Vinile, Categoria e Carrello

---
+++ src/main/java/com/progetto/viniliprogetto/Model/Carrello.java
@@ -0,0 +1,10 @@
+package com.progetto.viniliprogetto.Model;
+
+import java.util.ArrayList;
+
+public class Carrello {
+    private ArrayList<Vinile> vinile = new ArrayList<Vinile>();
+    private int totale = 0;
+    private int totprodotti = 0;
+
+}
