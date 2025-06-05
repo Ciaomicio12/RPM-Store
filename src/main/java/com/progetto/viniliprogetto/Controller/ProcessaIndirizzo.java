40eabb958cd4fba2b6f16e41afc06c4e6303a845
ggrauso7429 <g.grauso1@studenti.unisa.it>
2025-06-05 05:02:36 +0000

    Altri fix città

--- src/main/java/com/progetto/viniliprogetto/Controller/ProcessaIndirizzo.java
+++ src/main/java/com/progetto/viniliprogetto/Controller/ProcessaIndirizzo.java
@@ -3,6 +3,7 @@ package com.progetto.viniliprogetto.Controller;
 import com.progetto.viniliprogetto.Model.Indirizzo;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 public class ProcessaIndirizzo {
 
@@ -15,10 +16,11 @@ public class ProcessaIndirizzo {
         String cap = request.getParameter("codice-postale");
         String numeroCivico = request.getParameter("numero-civico");
         String telefono = request.getParameter("telefono");
+        Pattern digitPattern = Pattern.compile(".*\\d.*");
         String error = "";
         if (strada == null || strada.trim().isEmpty())
             error += "Campo strada non valido; ";
-        if (citta == null || citta.trim().isEmpty())
+        if (citta == null || citta.trim().isEmpty() || digitPattern.matcher(citta).matches())
             error += "Citta non valida; ";
         if (cap == null || cap.trim().isEmpty() || cap.length() > 6 || !cap.matches("\\d+"))
             error += "Il cap non deve essere vuoto";
